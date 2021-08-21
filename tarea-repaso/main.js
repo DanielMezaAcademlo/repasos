1a689b9b5aff9373135a7d422c4acc8febff7306
Santiago Flores <sanffp@hotmail.com>
2021-08-20 20:06:44 -0500

    Se creo un div y se añadio js

---
+++ tarea-repaso/main.js
@@ -0,0 +1,9 @@
+const pokemon = [
+    { name: "pikachu", type: "electric" },
+    { name: "charmander", type: "fire" },
+    { name: "eevee", type: "normal" },
+    { name: "squirtle", type: "water" }
+  ];
+  const div1 = document.getElementById("div1")
+  div1.style.height= "50px";
+  div1.style.backgroundColor = "black";
