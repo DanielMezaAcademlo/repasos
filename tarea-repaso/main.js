6d7308b1bb4c5982ac6afe7dff9273be83c56a74
Ricardo Rodriguez <ricardorod1147@gmail.com>
2021-08-20 20:36:30 -0500

    div principal con su clase

--- tarea-repaso/main.js
+++ tarea-repaso/main.js
@@ -4,12 +4,22 @@ const pokemon = [
   { name: "eevee", type: "normal" },
   { name: "squirtle", type: "water" }
 ];
+
+//DIV CONTENEDOR PRINCIPAL
 const div1 = document.getElementById("div1");
 
+//FUNCIÓN QUE RECORRE EL OBJETO
 const pokemonList = () => {
   pokemon.forEach(currentPokemon => {
     console.log(currentPokemon);
+    const pokemonContainer = document.createElement("div");
+    pokemonContainer.classList.add("pokemonElement");
+    div1.appendChild(pokemonContainer);
+
   });
 };
 
+//LLAMADO A LA FUNCIÓN
 pokemonList();
+
+
